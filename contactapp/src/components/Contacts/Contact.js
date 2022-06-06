c4c4b0ecfc427b09819a7cc8e90012a382b668fa
ismailcet <ismailcet3@gmail.com>
2022-06-06 16:50:28 +0300

    Contact App

---
+++ contactapp/src/components/Contacts/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import List from "./List/List";
+import Form from "./Form/Form";
+function Contact() {
+  return (
+    <div>
+      <List />
+      <Form />
+    </div>
+  );
+}
+
+export default Contact;
