9a68ac0570aac8ae9d73447e458d4423c5c59b31
ismailcet <ismailcet3@gmail.com>
2022-06-06 13:53:40 +0300

    useState Method

--- React-Dersleri/react-dersleri/src/App.js
+++ React-Dersleri/react-dersleri/src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import User from "./Components/User";
 function App() {
   let userName = "Ismail";
   let surName = "Cetin";
   let age = 15;
-  let isLoggedIn = true;
+  const [isLoggedIn, setLogged] = useState(false);
+  const [friendList, setFriendList] = useState([
+    "ayse",
+    "ahmet",
+    "osman",
+    "ismail",
+  ]);
   const friends = [
     {
       id: 1,
@@ -24,13 +30,26 @@ function App() {
     },
   ];
   return (
-    <User
-      name={userName}
-      surname={surName}
-      age={age}
-      isLoggedIn={isLoggedIn}
-      friends={friends}
-    />
+    <>
+      <User
+        name={userName}
+        surname={surName}
+        age={age}
+        isLoggedIn={isLoggedIn}
+        friends={friends}
+      />
+      <button onClick={() => setLogged(!isLoggedIn)}>Click Me</button>
+
+      <hr />
+      {friendList.map((friend) => {
+        return <h4>{friend}</h4>;
+      })}
+      <h3>Arkadaş Ekle</h3>
+      <br />
+      <button onClick={() => setFriendList([...friendList, "deneme"])}>
+        Click Friends
+      </button>
+    </>
   );
 }
 
